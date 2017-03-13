fad5cd5a6e0cfd831b49d6d139c4b4a03eb460ec
Ryan Mounce <ryan@mounce.com.au>
2017-03-14 01:39:20 +1030

    add hap ac lite

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 SCRIPT=$(readlink "$0")
 CONFIGPATH=$(dirname "$SCRIPT")
 
+if [ ! -f "$CONFIGPATH/$1" ]; then
+	echo "Can't find config file \"$1\""
+	exit
+fi
+
 cp "$CONFIGPATH/$1" .config
 make defconfig
 scripts/diffconfig.sh
